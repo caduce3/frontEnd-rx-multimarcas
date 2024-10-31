94e8779e3140caf1b51b0385c8ee3f3a0c2c87dc
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-10-31 20:08:40 -0300

    feat: adicionando onclick em cada compra do cliente para mostrar os detalhes

--- frontend-rx-multimarcas/src/pages/app/clientes/clientes-details/cliente-historico-compras.tsx
+++ frontend-rx-multimarcas/src/pages/app/clientes/clientes-details/cliente-historico-compras.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { formatDateTimeToPTBR } from "@/services/formated-data-pt-br";
 import { Search } from "lucide-react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useCallback } from "react";
 
 interface Compra {
     id: string;
@@ -28,14 +29,19 @@ const HistoricoComprasCliente = ({ cliente }: HistoricoComprasProps) => {
     const [searchParams, setSearchParams] = useSearchParams();
     const page = parseInt(searchParams.get('page') ?? '1', 10);
 
-    function handlePaginate(newPage: number) {
-        setSearchParams(prev => {
-            prev.set('page', newPage.toString());
-            return prev;
-        });
-    }
+    const navigate = useNavigate();
 
-    if (!cliente?.Carrinho) {
+    const handlePaginate = useCallback(
+        (newPage: number) => {
+            setSearchParams((prev) => {
+                prev.set('page', newPage.toString());
+                return prev;
+            });
+        },
+        [setSearchParams]
+    );
+
+    if (!cliente?.Carrinho || cliente.Carrinho.length === 0) {
         return <p className="text-gray-700 text-center">Nenhum histórico de compras encontrado</p>;
     }
 
@@ -46,25 +52,39 @@ const HistoricoComprasCliente = ({ cliente }: HistoricoComprasProps) => {
     const endIndex = startIndex + ITEMS_PER_PAGE;
     const comprasParaExibir = cliente.Carrinho.slice(startIndex, endIndex);
 
-    return ( 
+    const handleDetailsClick = (id: string) => {
+        navigate(`/vendas/${id}`);
+    };
+
+    return (
         <>
             <Card className="w-[39vw] p-2">
                 <CardHeader>
                     <CardTitle className="text-xl font-semibold">Histórico de compras</CardTitle>
-                    <CardDescription className="text-sm text-gray-600">Histórico de compras de {cliente?.nome}</CardDescription>
+                    <CardDescription className="text-sm text-gray-600">
+                        Histórico de compras de {cliente?.nome}
+                    </CardDescription>
                 </CardHeader>
                 <CardContent className="mt-2 mb-1 rounded-lg">
                     {comprasParaExibir.length > 0 ? (
-                        comprasParaExibir.map((compra, index) => (
-                            <div key={index} className="mb-4 p-4 bg-white border rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 flex justify-between items-center">
+                        comprasParaExibir.map((compra) => (
+                            <div
+                                key={compra.id}
+                                className="mb-4 p-4 bg-white border rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 flex justify-between items-center"
+                            >
                                 <div className="mb-2">
                                     <p className="text-lg font-bold">R$ {compra.valorTotal.toFixed(2)}</p>
-                                    <p className="text-sm text-gray-500">Data: {formatDateTimeToPTBR(compra.dateCreated)}</p>
+                                    <p className="text-sm text-gray-500">
+                                        Data: {formatDateTimeToPTBR(compra.dateCreated)}
+                                    </p>
                                 </div>
                                 <div>
-                                    <Button variant="default" size="sm">
+                                    <Button
+                                        variant="default"
+                                        size="sm"
+                                        onClick={() => handleDetailsClick(compra.id)}
+                                    >
                                         <Search className="h-4 w-4" />
-                                        <span className="sr-only">Detalhes do cliente</span>
                                     </Button>
                                 </div>
                             </div>
@@ -74,17 +94,16 @@ const HistoricoComprasCliente = ({ cliente }: HistoricoComprasProps) => {
                     )}
                 </CardContent>
                 {totalPages > 1 && (
-                    <Pagination 
-                        currentPage={page} 
-                        totalPages={totalPages} 
-                        totalItens={totalItems} 
+                    <Pagination
+                        currentPage={page}
+                        totalPages={totalPages}
+                        totalItens={totalItems} // Corrigido aqui
                         onPageChange={handlePaginate}
                     />
                 )}
             </Card>
-
         </>
     );
-}
+};
 
 export default HistoricoComprasCliente;
